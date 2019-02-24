97a94d9b528c1ec855dbebeebc66373628b1df0c
wachaon <wachaon@gmail.com>
2019-02-24 23:57:04 +0900

    core.json の行頭の空白を削除

--- src/build.js
+++ src/build.js
@@ -11,7 +11,7 @@ try {
     files.forEach( file => {
         let [ , filename, ext ] = file.name.match( /(.+)(\..+)/ )
         if ( ext !== '.js' ) return
-        let source = io.readFileSync( file.Path ).replace( /\r/g, '' )
+        let source = io.readFileSync( file.Path ).replace( /\r/g, '' ).replace( /^\s+/mg, '' )
         result[ filename ] = { source, mapping: {}, name: null }
     })
 
