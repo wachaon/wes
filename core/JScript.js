17d0c4e2a2ae1bd0276af0f675ecf2b81d12c87b
wachaon <wachaon@gmail.com>
2019-03-03 01:10:18 +0900

    core/JScriptを追加 code/vbs.jsをcore/VBScript.jsに変更

---
+++ core/JScript.js
@@ -0,0 +1,21 @@
+const { JScript } = require( 'sc' )
+
+JScript.AddCode(`
+function enumerator ( collection ) {
+    return new Enumerator( collection )
+}`)
+
+const Enumerator = new Proxy( () => {},{
+    construct( target, args ) {
+        const res = []
+        const e = JScript.Run( 'enumerator', args[0] )
+        for ( ; !e.atEnd(); e.moveNext() ) {
+            res.push( e.item() )
+        }
+        return res
+    }
+} )
+
+module.exports = {
+    Enumerator
+}
