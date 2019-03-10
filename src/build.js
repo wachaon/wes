c8dac5cae9bf8ee203a90f30b84c4ff70adb160f
wachaon <wachaon@gmail.com>
2019-03-10 15:52:48 +0900

    build.js

--- src/build.js
+++ src/build.js
@@ -15,6 +15,24 @@ files.forEach( file => {
 
 let graph = JSON.stringify( result, null, 4 )
 
+const log = require( 'log' )
+
+let template = io.readFileSync( 'src/template.js' )
+let sep = /\r\n/.test( template ) ? '\r\n': '\n'
+let line = template.split( sep )
+const match = /^\s*\/\*\s*includes\s+(.+)\s*\*\/\s*$/
+
+let res = line.map( value => {
+    if ( !( match.test( value ) ) ) {
+        return value
+    } else {
+        let includeSource = graph
+        return ',' + includeSource
+    }
+} ).join( sep )
+
+log( () => io.writeFileSync( 'wes.js', res ) )
+
 module.exports = graph
 
 
