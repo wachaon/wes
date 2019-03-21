3128c988408bc047cf84cb9143979b4ecf936ab2
wachaon <wachaon@gmail.com>
2019-03-21 19:12:22 +0900

    wes.js remove `option`

--- wes.js
+++ wes.js
@@ -1,15 +1,15 @@
 try {
     var WShell = WScript.CreateObject('WScript.Shell')
     var console = {
-        log: function () {
-            var args = Array.prototype.slice.call( arguments ).join( ' ' )
+        log: function() {
+            var args = Array.prototype.slice.call(arguments).join(' ')
             var res = args + console.ansi.clear
-            WScript.StdErr.WriteLine( res )
+            WScript.StdErr.WriteLine(res)
         },
         print: function(args) {
-            var args = Array.prototype.slice.call( arguments ).join( ' ' )
+            var args = Array.prototype.slice.call(arguments).join(' ')
             var res = args + console.ansi.clear
-            WScript.StdErr.Write( res )
+            WScript.StdErr.Write(res)
         },
         ansi: {
             clear: '\u001B[0m',
@@ -160,23 +160,19 @@ try {
                     "name": "lib/version"
                 }
             }
-        );
-        function require(id, option) {
+        )
+        function require(id) {
             if (graph[id] != null) {
                 if (!id.startsWith('{')) stack.push([null, null])
                 var code = graph[id].code
                 var source = graph[id].source
                 var mapping = graph[id].mapping
-                function localRequire(name, option) {
+                function localRequire(name) {
                     var res = mapping[name]
                     if (res == null)
-                        return require(name, option && !option.startsWith('_')
-                            ? option
-                            : null)
+                        return require(name)
                     else
-                        return require(res, option && !option.startsWith('_')
-                            ? option
-                            : null)
+                        return require(res)
                 }
                 localRequire.stack = stack
                 localRequire.graph = graph
@@ -188,16 +184,20 @@ try {
                     env: {
                         NODE_DEBUG: 'semver'
                     },
-                    argv: ( function() {
+                    argv: (function() {
                         var res = ['wes']
-                        for (var i = 0, args = WScript.Arguments; i < args.length; i++) {
+                        for (
+                            var i = 0, args = WScript.Arguments;
+                            i < args.length;
+                            i++
+                        ) {
                             res.push(WScript.Arguments.Item(i))
                         }
                         return res
                     })(),
                     versions: {
-                node: '8.0.0'
-                },
+                        node: '8.0.0'
+                    },
                     platform: 'win32'
                 }
                 var fn =
@@ -227,9 +227,7 @@ try {
                 return module.exports
             }
             try {
-                if (option && option.startsWith('_'))
-                    return WScript.CreateObject(id, option)
-                else return WScript.CreateObject(id)
+                return WScript.CreateObject(id)
             } catch (e) {}
             var io = require('io')
             var curr = (function() {
@@ -283,10 +281,7 @@ try {
                     io.fileExists(package))
                 ) {
                     var temp = JSON.parse(
-                        io.readFileSync(
-                            package,
-                            option && !option.startsWith('_') ? option : null
-                        )
+                        io.readFileSync( package )
                     ).main
                     if (temp != null) {
                         if (((res = io.join(value, temp)), io.fileExists(res)))
@@ -320,16 +315,13 @@ try {
             stack.push([entry, uuid])
             graph[uuid] = {
                 source: io
-                    .readFileSync(
-                        entry,
-                        option && !option.startsWith('_') ? option : null
-                    )
+                    .readFileSync(entry)
                     .replace(/\r/g, ''),
                 name: entry.match(/([^\/]+)$/)[0] + '',
                 mapping: {}
             }
             history.push([entry, uuid])
-            return require(uuid, option ? option : null)
+            return require(uuid)
         }
         var FSO = require('Scripting.FileSystemObject')
         var genUUID = function() {
