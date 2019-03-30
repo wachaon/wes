7634302bd080118b0b3acea7bfd9fbce5d47c4a5
wachaon <wachaon@gmail.com>
2019-03-30 23:43:02 +0900

    Fix test/Enumerator

--- test/Enumerator.js
+++ test/Enumerator.js
@@ -4,10 +4,25 @@ const { describe, it, assert } = require('test') // { describe, it, assert }
 describe('Enumerator test', () => {
     it('lib/ files', () => {
         const FSO = require('Scripting.FileSystemObject')
-
-        let files = new Enumerator(FSO.GetFolder(`./lib`).Files)
-        let list = files.map( (v) => `${ v }`)
-        assert.ng(JSON.stringify(list, null, 2))
-
+        let files = ( new Enumerator(FSO.GetFolder(`./lib`).Files) ).map( v => v.name )
+        let list = [
+            "args.js",
+            "day.js",
+            "debug.js",
+            "enumerator.js",
+            "event.js",
+            "io.js",
+            "JScript.js",
+            "log.js",
+            "output.js",
+            "pipe.js",
+            "sc.js",
+            "test.js",
+            "text.js",
+            "validation.js",
+            "VBScript.js",
+            "version.js"
+        ]
+        assert( JSON.stringify( files ) === JSON.stringify( list ) )
     })
 })
