76f82fae46550471fe8b1fea611e8eb4e8e7e717
wachaon <wachaon@gmail.com>
2019-05-05 18:56:23 +0900

    Improve src/build

--- src/build.js
+++ src/build.js
@@ -1,7 +1,7 @@
 const FSO = require( "Scripting.FileSystemObject" )
 const fs = require( 'filesystem' )
 const path = require( 'pathname' )
-const { REG_CRLF, CRLF, SPACE } = require( 'text' )
+const { REG_LINE_SEP, REG_CRLF, REG_CR, CRLF, SPACE } = require( 'text' )
 const { Enumerator } = require( 'JScript' )
 
 let files = new Enumerator( FSO.GetFolder( "lib" ).Files )
@@ -9,18 +9,15 @@ let files = new Enumerator( FSO.GetFolder( "lib" ).Files )
 let result = {}
 
 files.forEach( file => {
-    //let [ , filename, ext ] = file.name.match( /(.+)(\..+)/ )
     let filename = path.basename( file.name, '.js' )
     let ext = path.extname( file.name )
-    //console.log( filename, ext )
-    //console.log( path.basename( file.name, '.js' ), path.extname( file.name ) )
     if ( ext !== '.js' ) return
-    let source = fs.readTextFileSync( file.Path ).replace( /\r/g, '' ).replace( /^\s+/mg, '' )
+    let source = fs.readTextFileSync( file.Path ).replace( REG_CR, '' ).replace( /^\s+/mg, '' )
     result[ filename ] = { source, mapping: {}, name: `wes/${ filename }` }
 })
 
 let graph = ( ',' + JSON.stringify(result, null, 4) )
-    .split( REG_CRLF )
+    .split( REG_LINE_SEP )
     .map(v => SPACE.repeat(12) + v)
     .join( CRLF )
 
@@ -28,7 +25,7 @@ let graph = ( ',' + JSON.stringify(result, null, 4) )
 const log = require( 'log' )
 
 let template = fs.readTextFileSync( 'src/template.js' )
-let sep = /\r\n/.test( template ) ? '\r\n': '\n'
+let sep = REG_CRLF.test( template ) ? CRLF: LF
 let line = template.split( sep )
 const match = /^\s*\/\*\s*includes\s+(.+)\s*\*\/\s*$/
 
