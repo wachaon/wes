4477a05f169c8609f846fca0e9576dd6b89cfdd8
wachaon <wachaon@gmail.com>
2019-05-05 19:00:58 +0900

    Improve src/test

--- src/test.js
+++ src/test.js
@@ -1,9 +1,9 @@
 const { Enumerator } = require( 'JScript' )
-const io = require( 'io' )
+const fs = require( 'filename' )
 
 const FSO = require('Scripting.FileSystemObject')
 let files = ( new Enumerator(FSO.GetFolder( `./test` ).Files ) ).map( v => v.path )
 
-files.forEach( v => eval( io.readFileSync( v ) ) )
+files.forEach( v => eval( fs.readTextFileSync( v ) ) )
 
 console.log( JSON.stringify( console.debugLog, null, 2 ) )
