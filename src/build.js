9b251c72aee63aca29d139c6591f71cc16262564
wachaon <wachaon@gmail.com>
2019-05-05 18:51:38 +0900

    Improve src/build

--- src/build.js
+++ src/build.js
@@ -1,5 +1,7 @@
 const FSO = require( "Scripting.FileSystemObject" )
-const io = require( 'io' )
+const fs = require( 'filesystem' )
+const path = require( 'pathname' )
+const { REG_CRLF, CRLF, SPACE } = require( 'text' )
 const { Enumerator } = require( 'JScript' )
 
 let files = new Enumerator( FSO.GetFolder( "lib" ).Files )
@@ -7,21 +9,25 @@ let files = new Enumerator( FSO.GetFolder( "lib" ).Files )
 let result = {}
 
 files.forEach( file => {
-    let [ , filename, ext ] = file.name.match( /(.+)(\..+)/ )
+    //let [ , filename, ext ] = file.name.match( /(.+)(\..+)/ )
+    let filename = path.basename( file.name, '.js' )
+    let ext = path.extname( file.name )
+    //console.log( filename, ext )
+    //console.log( path.basename( file.name, '.js' ), path.extname( file.name ) )
     if ( ext !== '.js' ) return
-    let source = io.readFileSync( file.Path ).replace( /\r/g, '' ).replace( /^\s+/mg, '' )
-    result[ filename ] = { source, mapping: {}, name: `lib/${ filename }` }
+    let source = fs.readTextFileSync( file.Path ).replace( /\r/g, '' ).replace( /^\s+/mg, '' )
+    result[ filename ] = { source, mapping: {}, name: `wes/${ filename }` }
 })
 
 let graph = ( ',' + JSON.stringify(result, null, 4) )
-    .split(/\r?\n/)
-    .map(v => " ".repeat(12) + v)
-    .join( '\r\n' )
+    .split( REG_CRLF )
+    .map(v => SPACE.repeat(12) + v)
+    .join( CRLF )
 
 
 const log = require( 'log' )
 
-let template = io.readFileSync( 'src/template.js' )
+let template = fs.readTextFileSync( 'src/template.js' )
 let sep = /\r\n/.test( template ) ? '\r\n': '\n'
 let line = template.split( sep )
 const match = /^\s*\/\*\s*includes\s+(.+)\s*\*\/\s*$/
@@ -34,7 +40,7 @@ let res = line.map( value => {
     }
 } ).join( sep )
 
-log( () => io.writeFileSync( 'wes.js', res ) )
+log( () => fs.writeTextFileSync( 'wes.js', res ) )
 
 module.exports = graph
 
