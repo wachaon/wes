ff6939caefbb167d6baa5eb7befcdce38f8a2e68
wachaon <wachaon@gmail.com>
2019-05-31 06:02:25 +0900

    Remake lib/pipe.js

--- test/pipe.js
+++ test/pipe.js
@@ -1,4 +1,4 @@
-const pipe = require( '/lib/pipe' )
+const Pipe = require( '/lib/pipe' )
 const { describe, it, assert, pass } = require( '/lib/minitest' )
 
 
@@ -8,11 +8,11 @@ describe( '#test pipe', () => {
     const multi = ( curr ) => ( acc ) => acc * curr
     const div = ( curr ) => ( acc ) => acc / curr
     const rem = ( curr ) => ( acc ) => acc % curr
-    it( 'pipe( 3 ).pipe( add( 5 ) ).dist() === 8', () => {
-        assert( pipe( 3 ).pipe( add( 5 ) ).dist() === 8 )
+    it( 'pipe( 3 ).pipe( add( 5 ) ).value === 8', () => {
+        assert( new Pipe( 3 ).pipe( add( 5 ) ).value === 8 )
     } )
-    it ( 'pipe( 14 ).pipe( div( 7 ), add( 6 ), sub( 4 ), multi( 5 ), rem( 3 ) ).dist() === 2', () => {
-        assert( pipe( 14 ).pipe( div( 7 ), add( 6 ), sub( 4 ), multi( 5 ), rem( 3 ) ).dist() === 2 )
+    it ( 'pipe( 14 ).pipe( div( 7 ), add( 6 ), sub( 4 ), multi( 5 ), rem( 3 ) ).value === 2', () => {
+        assert( new Pipe( 14 ).pipe( div( 7 ), add( 6 ), sub( 4 ), multi( 5 ), rem( 3 ) ).value === 2 )
     } )
 } )
 
