aeda6328ce12802af488c38474251d719b90423e
wachaon <wachaon@gmail.com>
2020-09-08 21:05:29 +0900

    Add a process to translate READMEAdd a process to translate README

---
+++ src/lang.md
@@ -0,0 +1,12 @@
++  [*簡体字*](README.zh-CN.md) <!-- 中国語 (簡体字) -->
++  [*繁体字*](README.zh-TW.md) <!-- 中国語 (繁体字) -->
++  [*English*](README.en.md) <!-- 英語 -->
++  [*हिन्दी*](README.hi.md)　<!-- ヒンディー語 -->
++  [*Español*](README.es.md) <!-- スペイン語 -->
++  [*عربى*](README.ar.md) <!-- アラビア語 -->
++  [*বাংলা*](README.bn.md) <!-- ベンガル語 -->
++  [*Português*](README.pt.md) <!-- ポルトガル語 -->
++  [*русский язык*](README.ru.md) <!-- ロシア語 -->
++  [*Deutsch*](README.de.md) <!-- ドイツ語 -->
++  [*français*](README.fr.md) <!-- フランス語 -->
++  [*italiano*](README.it.md)　<!-- イタリア語 -->
---
+++ src/translate.js
@@ -0,0 +1,60 @@
+try {
+const charset =`簡体	zh-CN
+繁体	zh-TW
+英語	en
+ヒンディー語	hi
+スペイン語	es
+アラビア語	ar
+ベンガル語	bn
+ポルトガル語	pt
+ロシア語	ru
+ドイツ語	de
+フランス語	fr
+イタリア語	it
+日本語	ja`.split(/\r?\n/).map(str => str.split('\t'))
+
+//console.log('%O', charset)
+
+const inspect = require('unist-util-inspect')
+const remark = require('remark')()
+const { api } = require('/account').google_apps_script
+const md_translate = require('markdown-translate')(api, { source: 'ja'})
+const Action = require('markdown-actions')
+const { readTextFileSync, writeTextFileSync } = require('filesystem')
+const { resolve } = require('pathname')
+const visit = require('unist-util-visit')
+
+const readme = readTextFileSync(resolve(process.cwd(), 'docs/README.md'))
+
+charset.forEach(list => {
+    const lang = list[1]
+    let res
+    const spec = resolve(process.cwd(), `docs/README.${lang}.md`)
+    res = lang === 'ja' ? readme : md_translate(readme, { target: lang })
+    const action = new Action(res)
+    /*
+    visit(mdast, (node, index, parent) => {
+
+    })
+    */
+    action.on('imports', ({node, index, parent}, spec) => {
+        const md = readTextFileSync(spec)
+        const items = remark.parse(md)
+        parent.children.splice(index, 1, ...items.children)
+    })
+    action.run('translator')
+    const result = remark.stringify(action.mdast)
+    console.log(writeTextFileSync(spec, result, 'UTF-8N'))
+    if (lang === 'ja') {
+        visit(_mdast,(node, index, parent) => {
+            if (node.type === 'link') {
+                node.url = 'docs/' + node.url
+            }
+        })
+        writeTextFileSync(resolve(process.cwd(), `README.md`), remark.stringify(action.mdast), 'UTF-8N')
+    }
+})
+
+} catch(e) {
+    throw e
+}
