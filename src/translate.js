7b2202dfea242b7450d706f75ad8067c26f5c5f5
wachaon@gmail.com <wachaon@gmail.com>
2021-08-08 00:06:08 +0900

    Fix

--- src/translate.js
+++ src/translate.js
@@ -15,8 +15,8 @@ const charset = `簡体	zh-CN
     .map((str) => str.split('\t'))
 
 const remark = require('remark')()
-const { api } = require('/account').google_apps_script
-const md_translate = require('markdown-translate')(api, { source: 'ja' })
+const api = require('/account').google_apps_script.translate
+const md_translate = require('markdown-translate')
 const Action = require('markdown-actions')
 const { readTextFileSync, writeTextFileSync } = require('filesystem')
 const { resolve } = require('pathname')
@@ -28,7 +28,7 @@ charset.forEach((list) => {
     const lang = list[1]
     let res
     const spec = resolve(process.cwd(), `docs/README.${lang}.md`)
-    res = lang === 'ja' ? readme : md_translate(readme, { target: lang })
+    res = lang === 'ja' ? readme : md_translate(api, readme, { target: lang })
     const action = new Action(res)
 
     action.on('imports', ({ node, index, parent }, spec) => {
