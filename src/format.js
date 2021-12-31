42b6ca785a0bba659c86ad80256ba69bdb323bf4
wachaon@gmail.com <wachaon@gmail.com>
2022-01-01 07:44:17 +0900

    Output a signal to start

--- src/format.js
+++ src/format.js
@@ -1,9 +1,11 @@
 const { readdirSync, readdirsSync, readFileSync, writeFileSync } = require('filesystem')
 const { resolve } = require('pathname')
 const { format } = require('fmt')
+const { brightBlue } = require('ansi')
 
 const UTF8 = 'UTF-8N'
 
+console.log('%OStart formatting', brightBlue)
 function formatter(dir) {
     readdirsSync(resolve(process.cwd(), dir), UTF8)
         .filter((spec) => spec.type === 'file' && spec.path.endsWith('.js'))
