ebe51ab2d99acdb014c51ba27c9bba8e9130fbf1
wachaon@gmail.com <wachaon@gmail.com>
2022-01-01 20:03:54 +0900

    Avoid format babel due to large file size

--- src/format.js
+++ src/format.js
@@ -9,6 +9,7 @@ console.log('%SStart formatting', brightBlue)
 function formatter(dir) {
     readdirsSync(resolve(process.cwd(), dir), UTF8)
         .filter((spec) => spec.type === 'file' && spec.path.endsWith('.js'))
+        .filter((spec) => !spec.path.endsWith('babel-standalone.js'))
         .forEach((spec) => {
             const { path } = spec
             console.log('%O', writeFileSync(path, format(readFileSync(path, UTF8)), UTF8))
