e3a472aa085b446b85b887ac96d43224748dc249
wachaon@gmail.com <wachaon@gmail.com>
2022-02-20 11:15:01 +0900

    Specify with a relative path

--- src/build.js
+++ src/build.js
@@ -5,8 +5,8 @@ const { rLINE_SEP, rCRLF, rCR, CRLF, SPACE, NONE, LF } = require('text')
 const { Enumerator } = require('JScript')
 const { format } = require('fmt')
 
-require('src/format')
-const test = require('src/test')
+require('./format')
+const test = require('./test')
 if (!test) throw new Error('Must pass all tests')
 
 let files = new Enumerator(FSO.GetFolder('lib').Files)
@@ -22,7 +22,7 @@ files.forEach((file) => {
 })
 
 result['version'] = {
-    source: `module.exports = console.log('${require('package').version}')`,
+    source: `module.exports = console.log('${require('../package').version}')`,
     mapping: {},
     path: `{wes}/version`
 }
